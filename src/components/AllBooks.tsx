1fa8b4932f38174d793c815422a5d6734e91107c
=oaksas <=>
2024-02-07 16:17:20 +0300

    Add filter by title

--- src/components/AllBooks.tsx
+++ src/components/AllBooks.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Col, Divider, Row, Alert } from 'antd';
+import React, { useState } from 'react';
+import { Col, Divider, Row, Alert, Input } from 'antd';
 import Title from 'antd/es/typography/Title';
 import BookCard from './Cards/BookCard';
 import { useGetAllBooksQuery } from '../services/bookStoreApi';
@@ -8,6 +8,8 @@ import { Loader } from '.';
 
 const AllBooks: React.FC = () => {
     const { data: allBooks, isFetching, isError } = useGetAllBooksQuery();
+    const [filter, setFilter] = useState<string>('');
+
 
     if (isFetching) {
         return <Loader />;
@@ -19,14 +21,27 @@ const AllBooks: React.FC = () => {
     if (!allBooks) {
         return <Alert message="No books found" type="info" />;
     }
+    const filteredBooks = allBooks.filter((book: Book) =>
+        book.title.toLowerCase().includes(filter.toLowerCase())
+    );
 
     return (
         <>
             <Divider orientation="left">
                 <Title>All Books</Title>
             </Divider>
+            <Input
+                placeholder="Filter by Title"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}
+                style={{ margin: '16px', width: '300px' }}
+            />
+
             <Row gutter={[32, 32]}>
-                {allBooks?.map((book: Book) => (
+                {!filteredBooks.length && (
+                    <Alert message="No books found" type="info" style={{ width: '50%', margin: '20px' }} />
+                )}
+                {filteredBooks?.map((book: Book) => (
                     <Col key={book.id} xs={24} sm={12} md={8} lg={6}>
                         <BookCard book={book} />
                     </Col>
